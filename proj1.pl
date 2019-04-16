8e4dbef471b97539c1bd318d9f1400cf9a9a3e9c
antonioromeu <romeupinheiro27@gmail.com>
2019-04-16 17:40:12 +0100

    4th predicative working

--- proj1.pl
+++ proj1.pl
@@ -1,14 +1,19 @@
 % ----- Antonio Romeu Paulo Pinheiro, 92427 ----- %
 
+:- use_module(library(lists)).
+
 % ----- Predicados Auxiliares ----- %
 
 conta_ocur(_, [], 0).
 conta_ocur(P, [P | R], N) :-
-    number(P),
     conta_ocur(P, R, N1),
     !, N is N1 + 1.
 conta_ocur(P, [_ | R], N) :-
     conta_ocur(P, R, N).
+
+conta_num(P, L, N) :-
+    exclude(var, L, L1),
+    conta_ocur(P, L1, N).
 
 conta_variaveis(L, Cont) :-
     include(var, L, L_vars),
@@ -48,6 +53,18 @@ preenche_lista(L, Res) :-
     (L == Res_Temp, Res = Res_Temp;
      preenche_lista(Res_Temp, Res)).
 
+verifica_se_quantidade_ultrapassa(Fila, D) :-
+    conta_num(D, Fila, N), length(Fila, C), Comp is C / 2, N =< Comp.
+
+verifica_se_sao_metade(Fila, D) :-
+    conta_num(D, Fila, N), length(Fila, C), Comp is C / 2, N == Comp.
+
+adiciona_elementos(_, [], L, L).
+adiciona_elementos(El, [P | L], N_L, N_L1) :-
+    (number(P), append(N_L, [P], N_L0);
+    \+number(P), append(N_L, [El], N_L0)),
+    adiciona_elementos(El, L, N_L0, N_L1).
+
 % ----- Predicados Principais ----- %
 
 aplica_R1_triplo(Triplo, N_Triplo) :-
@@ -59,4 +76,8 @@ aplica_R1_fila_aux(Fila, N_Fila) :-
 aplica_R1_fila(Fila, N_Fila) :-
     preenche_lista(Fila, N_Fila).
 
-% ----- Outros Predicados ----- %
+aplica_R2_fila(Fila, N_Fila) :-
+    verifica_se_quantidade_ultrapassa(Fila, 0), verifica_se_quantidade_ultrapassa(Fila, 1),
+    (verifica_se_sao_metade(Fila, 0), adiciona_elementos(1, Fila, [], N_Fila);
+    verifica_se_sao_metade(Fila, 1), adiciona_elementos(0, Fila, [], N_Fila);
+    Fila = N_Fila).
