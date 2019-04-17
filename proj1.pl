cadd0ac566596928ce7ba748154037d652bb459c
antonioromeu <romeupinheiro27@gmail.com>
2019-04-17 15:26:05 +0100

    halfway through 6th predicative

--- proj1.pl
+++ proj1.pl
@@ -21,13 +21,14 @@ conta_variaveis(L, Cont) :-
 
 preenche_triplo([X, Y, Z], Res) :-
     conta_variaveis([X, Y, Z], Num_Vars),
-    (Num_Vars == 1, preenche_1([X, Y, Z], Res);
-    Num_Vars == 0, (conta_ocur(1, [X, Y, Z], N), N =\= 3, N > 0;
+    (Num_Vars == 0, (conta_ocur(1, [X, Y, Z], N), N =\= 3, N > 0;
                     conta_ocur(0, [X, Y, Z], N), N =\= 3, N > 0),
                     Res = [X, Y, Z];
     Num_Vars == 0, (conta_ocur(1, [X, Y, Z], N), N == 3, false;
                     conta_ocur(0, [X, Y, Z], N), N == 3, false);
-    Num_Vars == 2, Res = [X, Y, Z]).
+    Num_Vars == 1, preenche_1([X, Y, Z], Res);
+    Num_Vars == 2, Res = [X, Y, Z];
+    Num_Vars == 3, Res = [X, Y, Z]).
 
 preenche_1([X, Y, Z], Res) :-
     (var(X), NX is 2 - Z - Y, (NX == 2, Res = [1, Y, Z];
@@ -65,6 +66,37 @@ adiciona_elementos(El, [P | L], N_L, N_L1) :-
     \+number(P), append(N_L, [El], N_L0)),
     adiciona_elementos(El, L, N_L0, N_L1).
 
+escreve_Puzzle(Puz) :-
+    maplist(escreve_Linha, Puz).
+
+escreve_Linha([]) :- nl, !.
+
+escreve_Linha([P | R]) :-
+    (var(P) -> write('- ')
+            ;
+     write(P), write(' ')),
+     escreve_Linha(R).
+
+aplica_R1_R2_linhas([], N_Puz, N_Puz).
+aplica_R1_R2_linhas([P | R], N_Puz, N_PuzAux) :-
+    aplica_R1_R2_fila(P, N_L),
+    append(N_Puz, [N_L], N_Puz1),
+    aplica_R1_R2_linhas(R, N_Puz1, N_PuzAux).
+
+transpose([[]|_], []).
+transpose(Matrix, [Row|Rows]) :-
+    transpose_1st_col(Matrix, Row, RestMatrix),
+    transpose(RestMatrix, Rows).
+
+transpose_1st_col([], [], []).
+transpose_1st_col([[H|T]|Rows], [H|Hs], [T|Ts]) :-
+    transpose_1st_col(Rows, Hs, Ts).
+
+aplica_R1_R2_colunas(L, N_Puz, N_PuzAux) :-
+    transpose(L, L_Transposta),
+    aplica_R1_R2_linhas(L_Transposta, N_Puz, N_PuzAux),
+    transpose(N_PuzAux, N_Puz).
+
 % ----- Predicados Principais ----- %
 
 aplica_R1_triplo(Triplo, N_Triplo) :-
@@ -84,3 +116,14 @@ aplica_R2_fila(Fila, N_Fila) :-
 
 aplica_R1_R2_fila(Fila, N_Fila) :-
     aplica_R1_fila(Fila, N_Fila0) -> aplica_R2_fila(N_Fila0, N_Fila); false.
+
+aplica_R1_R2_puzzle(Puz, N_Puz) :-
+    aplica_R1_R2_linhas(Puz, [], N_Puz0),
+    aplica_R1_R2_colunas(N_Puz0, [], N_Puz).
+
+inicializa(Puz, N_Puz) :-
+    aplica_R1_R2_puzzle(Puz, N_Puz).
+
+% ----- Predicados Teste ----- %
+
+% [[0,_,_,1],[0,_,_,1],[1,_,_,_],[1,0,_,_]]
